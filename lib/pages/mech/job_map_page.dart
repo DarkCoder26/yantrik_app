d406f508e92cbcaf295da1792a2ba39220a39777
Preetinder Singh <preetindersingh13per@gmail.com>
2024-04-07 08:47:02 +0530

    mech map updated

--- lib/pages/mech/job_map_page.dart
+++ lib/pages/mech/job_map_page.dart
@@ -73,6 +73,7 @@ class _JobMapPageState extends State<JobMapPage> {
           },
           onStyleLoadedCallback: () {
             addMarker();
+            addPolyline();
           },
         ),
       ),
@@ -102,5 +103,38 @@ class _JobMapPageState extends State<JobMapPage> {
     mapController.addSymbol(const SymbolOptions(
         geometry: LatLng(28.35778988630296, 75.58279199607256),
         iconImage: "icon"));
+  }
+
+  void addPolyline() async {
+    List<LatLng> latlng = const <LatLng>[
+      LatLng(28.705436, 77.100462),
+      LatLng(28.705191, 77.100784),
+      LatLng(28.704646, 77.101514),
+      LatLng(28.704194, 77.101171),
+      LatLng(28.704083, 77.101066),
+      LatLng(28.703900, 77.101318),
+    ];
+    LatLngBounds latLngBounds = boundsFromLatLngList(latlng);
+    mapController.animateCamera(CameraUpdate.newLatLngBounds(latLngBounds));
+    mapController.addLine(
+        LineOptions(geometry: latlng, lineColor: "#3bb2d0", lineWidth: 4));
+  }
+
+  boundsFromLatLngList(List<LatLng> list) {
+    assert(list.isNotEmpty);
+    double? x0, x1, y0, y1;
+    for (LatLng latLng in list) {
+      if (x0 == null || x1 == null || y0 == null || y1 == null) {
+        x0 = x1 = latLng.latitude;
+        y0 = y1 = latLng.longitude;
+      } else {
+        if (latLng.latitude > x1) x1 = latLng.latitude;
+        if (latLng.latitude < x0) x0 = latLng.latitude;
+        if (latLng.longitude > y1) y1 = latLng.longitude;
+        if (latLng.longitude < y0) y0 = latLng.longitude;
+      }
+    }
+    return LatLngBounds(
+        northeast: LatLng(x1!, y1!), southwest: LatLng(x0!, y0!));
   }
 }
